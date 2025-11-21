46b1e9baf73427a3d3130a077044412f79a3c7f2
10laurenpatt <10laurenpatt@gmail.com>
2025-11-20 20:01:20 -0500

    NavBar

---
+++ components/Home/Navbar/MobileNav.tsx
@@ -0,0 +1,24 @@
+import { navLinks } from "@/constant/constant";
+import Link from "next/link";
+import React from "react"
+import { CgClose } from "react-icons/cg";
+
+const MobileNav = () => {
+    return <div>
+        {/* Overlay - adds a tint to de-focus all background elements */}
+        <div className='fixed inset-0 z-[1000] bg-black opacity-70 w-full h-screen'></div>
+        {/*  Nav Links */}
+        <div className='bg-[#0f0715] text-white fixed justify-center flex flex-col h-full w-[80%] sm:w-[60%]  space-y-6 z-[1006]'>
+            {navLinks.map((navlink) => {
+                return (<Link key={navlink.id} href={navlink.url}>
+                    <p className="nav__link text-[20px] ml-12 border-white border-b-[1.5px] pb-2 sm:text-[30px]">{navlink.label}</p>
+                </Link>
+                );
+            })}
+            {/* Close Button */}
+            <CgClose className='absolute top-[1.7rem] right-[1.4rem] sm:w-8 sm:h-8 w-6 h-6 text-white'/>
+        </div>
+    </div>
+}
+
+export default MobileNav;
