477e63be4530ab0364e3ffa20f6818c23b63ebcf
10laurenpatt <10laurenpatt@gmail.com>
2025-11-20 23:15:54 -0500

    Created UI for Homepage Hero Section

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,39 @@
+import { BaseInfo } from '@/data/data';
+import React from 'react';
+import { FaDownload } from 'react-icons/fa';
+import Image from 'next/image';
+
+const Hero = () => {
+    return (
+        <div className='w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative'>
+            <div className='flex justify-center flex-col w-4/5 h-full mx-auto'>
+                <div className='grid grid-cols-1 lg:grid-cols-2 items-center text-center sm:text-left gap-12'>
+                    {/* Text Content */}
+                    <div className='align'>
+                        <h1 className='text-2xl md:text-3xl lg:text-4xl mb-5 text-gray-300 font-semibold'>
+                            {BaseInfo.name}
+                        </h1>
+                        {/* Title */}
+                        <h1 className='text-bg bg-linear-to-r from-[#8750f7] to-white text-transparent bg-clip-text inline-block text-3xl sm:text-4xl md:text-5xl mg:text-6xl xl:text-7xl font-bold md:leading-[3.5rem] xl:leading-[4rem]'>
+                            {BaseInfo.position}
+                        </h1>
+                        {/* Description */}
+                        <p className='mt-6 text-sm md:text-base text-white opacity-60'>{BaseInfo.description}</p>
+
+                        {/* Resume Download Button */}
+                        <button className='md:px-8 md:py-2.5 px-6 py-1.5 text-white font-semibold text-sm md:text-lg transition-all duration-200 rounded-lg mt-8 bg-blue-700 hover:bg-blue-900 flex justify-self-center sm:justify-self-start space-x-2'>
+                            <span>Download Resume</span>
+                            <FaDownload />
+                        </button>
+                    </div>
+                    {/* Image Content */}
+                    <div className='mx-auto hidden lg:block rounded-[3rem] border-[3.5px] border-blue-950 overflow-x-hidden'>
+                        <Image src={BaseInfo.profilePic} alt={BaseInfo.name} width={700} height={700}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
