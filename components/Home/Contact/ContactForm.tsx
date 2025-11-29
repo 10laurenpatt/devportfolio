f4cade0764cac4b8fb42f1f88300ab86b6b35260
10laurenpatt <10laurenpatt@gmail.com>
2025-11-29 13:12:02 -0500

    Created Contact Form and Functionality for Localhost

---
+++ components/Home/Contact/ContactForm.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const ContactForm = () => {
+    return <div className='bg-[#140c1c] rounded-lg p-4 sm:p-10'>
+        <h1 className='text-bg bg-linear-to-r from-[#8750f7] to-white text-transparent bg-clip-text inline-block text-3xl sm:text-4xl md:text-5xl mg:text-6xl xl:text-7xl font-bold md:leading-[3.5rem] xl:leading-[4rem]'>
+            Let's Work Together
+        </h1>
+        <p className='text-gray-200 mt-3 lg:text-base text-xs md:text-sm'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea beatae quia excepturi corrupti repellendus. Aliquid!
+        </p>
+        {/* Input Fields */}
+        <form className='mt-8 block w-full overflow-hidden' action="https://api.web3forms.com/submit" method="POST">
+            <input type="hidden" name="access_key" value="0ad89dff-aa1b-42f1-b6d6-6241af10b90b"></input>
+            <div className='flex flex-col md:flex-row items-center justify-between gap-4'>
+                <input
+                    name='firstname'
+                    type='text'
+                    placeholder='First Name'
+                    className='flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1px] 
+                    border-gray-200/15 outline-none w-full' />
+                <input
+                    name='lastname'
+                    type='text'
+                    placeholder='Last Name'
+                    className='flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1px] 
+                    border-gray-200/15 outline-none w-full' />
+            </div>
+            <div className='flex flex-col md:flex-row items-center justify-between mt-5 gap-4'>
+                <input
+                    name='email'
+                    type='email'
+                    placeholder='Email Address'
+                    className='flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1px] 
+                    border-gray-200/15 outline-none w-full' />
+                <input
+                    name='phone'
+                    type='text'
+                    placeholder='Phone Number'
+                    className='flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-3 rounded-md border-[1px] 
+                    border-gray-200/15 outline-none w-full' />
+            </div>
+            <textarea
+                name='message'
+                rows={7}
+                placeholder='Enter message here...'
+                className='w-full mt-5 bg-black text-white placeholder:text-gray-600 px-4 py-3.5 rounded-md border-[1px]
+                border-gray-200/15 outline-none'></textarea>
+            <div className='mt-4'>
+                <button type='submit'
+                    className='px-8 py-3.5 bg-[#7947df] text-white hover:bg-[#5c2fb7] transition-all duration-300
+                    rounded-full'>Send Message</button>
+            </div>
+        </form>
+    </div>
+}
+
+export default ContactForm;
--- components/Home/Skills/SkillCard.tsx
+++ components/Home/Skills/SkillCard.tsx
@@ -3,19 +3,30 @@ import React from "react";
 
 type Props = {
     skill: {
-    id: number;
-    title: string;
-    image: string;
-    percent: string;
-}
+        id: number;
+        title: string;
+        image: string;
+        percent: string;
+    }
 }
 
-const SkillCard = ({skill} : Props) => {
+const SkillCard = ({ skill }: Props) => {
 
-    const {title, image} = skill
+    const { title, image } = skill
+
     return <div className='p-6 bg-gray-900 hover:bg-blue-900 duration-300 transition-all cursor-pointer text-center rounded-lg'>
-        <Image src={image} alt={title} width={80} height={80} className="object-cover mx-auto"/>
-        <h1 className='text-[18px] mt-4 text-white font-[600]'>{title}</h1>
+        {image.toString().includes('nextjs') ?
+            <div>
+                <div className='bg-gray-100 rounded-full w-[80px] h-[80px] p-1 flex justify-self-center'>
+                    <Image src={image} alt={title} width={80} height={80} className="object-cover mx-auto" />
+                </div>
+                <h1 className='text-[18px] mt-4 text-white font-[600]'>{title}</h1>
+            </div>
+            : <div>
+                <Image src={image} alt={title} width={80} height={80} className="object-cover mx-auto" />
+                <h1 className='text-[18px] mt-4 text-white font-[600]'>{title}</h1>
+            </div>}
+
     </div>
 
 }
