e8742e9850b697d7ecb2e3f42f4b89d94b081dba
10laurenpatt <10laurenpatt@gmail.com>
2026-01-17 11:01:50 -0500

    Wired Sanity to Hero

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -3,8 +3,14 @@ import React from 'react';
 import { FaDownload } from 'react-icons/fa';
 import Image from 'next/image';
 import Link from 'next/link';
+import { getHero } from '@/sanity/sanity-utils';
+import { PortableText } from 'next-sanity';
 
-const Hero = () => {
+export default async function Hero() {
+
+    const hero = await getHero();
+    console.log(hero)
+
     return (
         <div className='w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative'>
             <div className='flex justify-center flex-col w-1/2 h-full mx-auto'>
@@ -16,13 +22,17 @@ const Hero = () => {
                         </h1> */}
                         {/* Title */}
                         <h1 className='text-bg bg-linear-to-r from-[#8750f7] to-white text-transparent bg-clip-text inline-block mb-3 text-3xl sm:text-4xl md:text-5xl mg:text-6xl xl:text-7xl font-bold md:leading-[3.5rem] xl:leading-[4rem]'>
-                            {BaseInfo.position}
+                            {hero[0].title1}
                         </h1>
                         <h1 className='text-bg bg-linear-to-r from-[#8750f7] to-white text-transparent bg-clip-text inline-block text-3xl sm:text-4xl md:text-5xl mg:text-6xl xl:text-7xl font-bold md:leading-[3.5rem] xl:leading-[4rem]'>
-                            Software Developer
+                            {hero[0].title2}
                         </h1>
                         {/* Description */}
-                        <p className='mt-6 text-sm sm:text-md md:text-xl text-white opacity-80'>{BaseInfo.description}</p>
+                        {hero[0]?.description ? (
+                            <div className='prose mt-6 text-sm sm:text-md md:text-xl text-white opacity-80'>
+                                <PortableText value={hero[0].description} />
+                            </div>
+                        ) : null}
 
                         {/* Resume Download Button */}
                         <a 
@@ -43,5 +53,3 @@ const Hero = () => {
         </div>
     )
 }
-
-export default Hero;
